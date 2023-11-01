56b13c31291e2c2d494e01cd021bdc71aef9cd46
Krish-Parekh <krishparekh303@gmail.com>
2023-11-01 21:02:41 +0530

    fix: code-cleanup for auth-screen layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,44 +1,25 @@
-"use client";
-
 import React from "react";
 import clsx from "clsx";
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
-import { motion } from "framer-motion";
-import Image from "next/image";
-import AuthBannerImage from "@/public/AuthBannerImage.svg";
 import "@/app/styles/globals.css";
+import { AuthScreenClassName } from "@/lib/strings";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({
+export default function AuthScreenLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const dividerClassName = clsx(
-    inter.className,
-    "min-h-screen flex flex-col md:flex-row"
-  );
   return (
     <html lang="en">
       <body>
-        <div className={dividerClassName}>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            className="flex w-1/2 items-center justify-center"
-          >
+        <div className={clsx(inter.className, AuthScreenClassName)}>
+          <div className="flex w-1/2 items-center justify-center">
             {children}
-          </motion.div>
-          <div className="flex h-screen w-1/2 items-center justify-center bg-black">
-            <Image
-              src={AuthBannerImage}
-              alt="Auth Banner Image"
-              width={750}
-              height={750}
-            />
           </div>
+          <div className="w-1/2 bg-black"></div>
         </div>
         <Toaster />
       </body>
---
+++ lib/strings.ts
@@ -0,0 +1,1 @@
+export const AuthScreenClassName = "min-h-screen flex flex-col md:flex-row";
