d5cdd894da3e4e269dfa131dbdc1cbfe84069a9d
Krish-Parekh <krishparekh303@gmail.com>
2023-11-01 22:06:51 +0530

    feat: made form component for both login and signup

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,53 +1,13 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Input, PasswordInput } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { useToast } from "@/components/ui/use-toast";
+import { LoginForm } from "@/components/Auth/Form/LoginForm";
 
 export default function LoginPage() {
-  const { toast } = useToast();
   return (
-    <div className="w-[50%] space-y-10">
+    <div className="w-1/2 space-y-10">
       <h1 className="text-2xl font-bold">LOGO.</h1>
       <p className="text-xl font-semibold">
         Welcome back, please login to your account.
       </p>
-      <form className="space-y-6">
-        <div className="space-y-3">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="Email" />
-        </div>
-        <div className="space-y-3">
-          <Label htmlFor="password">Password</Label>
-          <PasswordInput id="password" placeholder="Password" />
-        </div>
-        <div className="flex items-center justify-between">
-          <div className="flex items-center gap-x-2">
-            <Checkbox id="remember-me" />
-            <Label htmlFor="remember-me">Remember me</Label>
-          </div>
-          <p className="text-sm text-black">Forgot Password ?</p>
-        </div>
-        <Button
-          type="submit"
-          className="w-full"
-          onClick={(e) => {
-            e.preventDefault();
-            toast({
-              title: "Login Successful",
-              description: "Welcome back",
-            });
-          }}
-        >
-          Login
-        </Button>
-        <p className="text-center text-sm text-slate-400">
-          Don&apos;t have an account?{" "}
-          <span className="font-semibold text-black">Signup.</span>
-        </p>
-      </form>
+      <LoginForm />
     </div>
   );
 }
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,36 +1,11 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { SignupForm } from "@/components/Auth/Form/SignupForm";
 
 export default function SignupPage() {
   return (
-    <div className="w-[50%] space-y-10">
+    <div className="w-1/2 space-y-10">
       <h1 className="text-2xl font-bold">LOGO.</h1>
       <p className="text-xl font-semibold">Create an account to get started.</p>
-      <form className="space-y-6">
-        <div className="space-y-3">
-          <Label htmlFor="username">Username</Label>
-          <Input id="username" type="text" placeholder="Username" />
-        </div>
-        <div className="space-y-3">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="Email" />
-        </div>
-        <div className="space-y-3">
-          <Label htmlFor="password">Password</Label>
-          <Input id="password" type="password" placeholder="Password" />
-        </div>
-        <Button type="submit" className="w-full">
-          Sign up
-        </Button>
-        <p className="text-center text-sm text-slate-400">
-          Already have an account?{" "}
-          <span className="font-semibold text-black">Login.</span>
-        </p>
-      </form>
+      <SignupForm />
     </div>
   );
 }
